36fea8c94c141a03e798461853e2bcd3afdebd92
Barrios Adriel Omar <barrios.adri.96@gmail.com>
2025-06-28 02:08:28 -0300

    pequeña modificacion en los ejercicios 1 y 2. creacion de la carpeta Ejercicios-6

--- Ejercicio-1/script.js
+++ Ejercicio-1/script.js
@@ -1,75 +1,70 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // Nos aseguramos que todos los elementos HTML esten disponibles
-    // usamos referencias a los HTML usando IDs, esto nos permite manipularlos
-    // desde JavaScript
-    const numero1Input = document.getElementById('numero1');
-    const numero2Input = document.getElementById('numero2');
-    const operacionSelect = document.getElementById('operacion');
-    const botonCalcular = document.getElementById('botonCalcular');
-    const resultadoDiv = document.getElementById('resultado');
-    // analizamos si la divios es por cero
-    const divisionPorCero = () => {
-        const operacion = operacionSelect.value; //obtenemos el valor de la operacion
-        const num2 = parseFloat(numero2Input.value); // volvemos flotante el segundo input
+const numero1Input = document.getElementById('numero1');
+const numero2Input = document.getElementById('numero2');
+const operacionSelect = document.getElementById('operacion');
+const botonCalcular = document.getElementById('botonCalcular');
+const resultadoDiv = document.getElementById('resultado');
+// analizamos si la divios es por cero
+const divisionPorCero = () => {
+    const operacion = operacionSelect.value; //obtenemos el valor de la operacion
+    const num2 = parseFloat(numero2Input.value); // volvemos flotante el segundo input
 
-        if (operacion === 'division' && num2 === 0) {
-            botonCalcular.disabled = true; //deshabilita el boton
-            resultadoDiv.textContent = "POR CERO IMPOSIBLE!"; // mostramos el mensaje al usuario.
-        } else {
-            botonCalcular.disabled = false; //si no se cumple, habilita el boton.
-            resultadoDiv.textContent = ''; //limpiamos cualquie mensaje que tengamos.
-        }
-    };
+    if (operacion === 'division' && num2 === 0) {
+        botonCalcular.disabled = true; //deshabilita el boton
+        resultadoDiv.textContent = "POR CERO IMPOSIBLE!"; // mostramos el mensaje al usuario.
+    } else {
+        botonCalcular.disabled = false; //si no se cumple, habilita el boton.
+        resultadoDiv.textContent = ''; //limpiamos cualquie mensaje que tengamos.
+    }
+};
 
-    // colocamos un evento en cada input y el boton
-    // actualizamos la funcion 'divisionPorCero' 
-    numero1Input.addEventListener('input', divisionPorCero);
-    numero2Input.addEventListener('input', divisionPorCero);
-    operacionSelect.addEventListener('change', divisionPorCero);
+// colocamos un evento en cada input y el boton
+// actualizamos la funcion 'divisionPorCero' 
+numero1Input.addEventListener('input', divisionPorCero);
+numero2Input.addEventListener('input', divisionPorCero);
+operacionSelect.addEventListener('change', divisionPorCero);
 
-    botonCalcular.addEventListener('click', () => {
-        // convertimos los valores del input en numeros
-        const num1 = parseFloat(numero1Input.value);
-        const num2 = parseFloat(numero2Input.value);
-        // obtenemos la operacion que el usuario seleccione
-        const operacion = operacionSelect.value;
+botonCalcular.addEventListener('click', () => {
+    // convertimos los valores del input en numeros
+    const num1 = parseFloat(numero1Input.value);
+    const num2 = parseFloat(numero2Input.value);
+    // obtenemos la operacion que el usuario seleccione
+    const operacion = operacionSelect.value;
 
-        resultadoDiv.textContent = ''; // Limpiamos errores o mensajes
+    resultadoDiv.textContent = ''; // Limpiamos errores o mensajes
 
-        if (isNaN(num1) || isNaN(num2)) {
-            resultadoDiv.textContent = 'Ingrese numeros validos.';
-            return; 
-        }
+    if (isNaN(num1) || isNaN(num2)) {
+        resultadoDiv.textContent = 'Ingrese numeros validos.';
+        return; 
+    }
 
-        let resultado; // almacen el resulado
+    let resultado; // almacen el resulado
 
-        //creamos un switch para realizar cada operacion
-        switch (operacion) {
-            case 'suma':
-                resultado = num1 + num2;
-                break;
-            case 'resta':
-                resultado = num1 - num2;
-                break;
-            case 'multiplicacion':
-                resultado = num1 * num2;
-                break;
-            case 'division':
-            // no es necesario, pero por las dudas hacemos una doble verificacion
-            // a la divison por 0    
-            if (num2 === 0) {
-                    resultadoDiv.textContent = 'ERROR, no es posible dividir por 0.';
-                    return; //detiene la ejecucion
-                }
-                resultado = num1 / num2;
-                break;
-            default:
-                resultadoDiv.textContent = 'Operacion invalida, pruebe de nuevo.';
-                return;
-        }
+    //creamos un switch para realizar cada operacion
+    switch (operacion) {
+        case 'suma':
+            resultado = num1 + num2;
+            break;
+        case 'resta':
+            resultado = num1 - num2;
+            break;
+        case 'multiplicacion':
+            resultado = num1 * num2;
+            break;
+        case 'division':
+        // no es necesario, pero por las dudas hacemos una doble verificacion
+        // a la divison por 0    
+        if (num2 === 0) {
+                resultadoDiv.textContent = 'ERROR, no es posible dividir por 0.';
+                return; //detiene la ejecucion
+            }
+            resultado = num1 / num2;
+            break;
+        default:
+            resultadoDiv.textContent = 'Operacion invalida, pruebe de nuevo.';
+            return;
+    }
 
-        resultadoDiv.textContent = `Resultado: ${resultado}`; //resultado final
-    });
-    
-    divisionPorCero();
+    resultadoDiv.textContent = `Resultado: ${resultado}`; //resultado final
 });
+    
+divisionPorCero();
--- Ejercicio-2/script.js
+++ Ejercicio-2/script.js
@@ -1,65 +1,62 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log("DOM Cargado y script iniciado.");
-    const palabrasOriginales = [
-        "River",
-        "Boca",
-        "PSG",
-        "Real Madrid",
-        "Barcelona",
-    ];
+const palabrasOriginales = [
+    "River",
+    "Boca",
+    "PSG",
+    "Real Madrid",
+    "Barcelona",
+];
 
-    // El script se carga al final del body, por eso el DOM ya esta disponible
-    const filtroInput = document.getElementById('filtroInput');
-    const botonFiltrar = document.getElementById('botonFiltrar');
-    const listaPalabrasUl = document.getElementById('listaPalabras');
-    const mensajeErrorDiv = document.getElementById('mensajeError');
+// El script se carga al final del body, por eso el DOM ya esta disponible
+const filtroInput = document.getElementById('filtroInput');
+const botonFiltrar = document.getElementById('botonFiltrar');
+const listaPalabrasUl = document.getElementById('listaPalabras');
+const mensajeErrorDiv = document.getElementById('mensajeError');
 
-    // mostramos la lista de palabras en el HTML.
-    const mostrarPalabras = (palabrasAMostrar) => {
-        listaPalabrasUl.innerHTML = '';// limpiamos la lista
+// mostramos la lista de palabras en el HTML.
+const mostrarPalabras = (palabrasAMostrar) => {
+    listaPalabrasUl.innerHTML = '';// limpiamos la lista
 
-        if (palabrasAMostrar.lenght === 0) {
-            const li = document.createElement('li');
-            li.textContent = 'no hay coincidencia.';
-            listaPalabrasUl.appendChild(li);
-            return;
-        }
+    if (palabrasAMostrar.lenght === 0) {
+        const li = document.createElement('li');
+        li.textContent = 'no hay coincidencia.';
+        listaPalabrasUl.appendChild(li);
+        return;
+    }
 
-        palabrasAMostrar.forEach(palabra => {
-            const li = document.createElement('li');
-            li.textContent = palabra;
-            listaPalabrasUl.appendChild(li);
-        });
-    };
+    palabrasAMostrar.forEach(palabra => {
+        const li = document.createElement('li');
+        li.textContent = palabra;
+        listaPalabrasUl.appendChild(li);
+    });
+};
 
-    // filtramos las palabras con las que el usuario ingresa
-    const filtrarLista = () => {
-        // obtenemos el texto y eliminamos espacion en blanco
-        const textoFiltro = filtroInput.value.trim();
+// filtramos las palabras con las que el usuario ingresa
+const filtrarLista = () => {
+    // obtenemos el texto y eliminamos espacion en blanco
+    const textoFiltro = filtroInput.value.trim();
 
-        // si el campo de texto esta vacio, mostramos un error
-        if (textoFiltro === '') {
-            mensajeErrorDiv.textContent = 'INGRESE UN TEXTO PARA FILTRAR';
-            mostrarPalabras(palabrasOriginales); // muestra las palabras
-            return;
-        } else {
-            mensajeErrorDiv.textContent = ''; // limpiamos el mensaje de error.
-        }
+    // si el campo de texto esta vacio, mostramos un error
+    if (textoFiltro === '') {
+        mensajeErrorDiv.textContent = 'INGRESE UN TEXTO PARA FILTRAR';
+        mostrarPalabras(palabrasOriginales); // muestra las palabras
+        return;
+    } else {
+        mensajeErrorDiv.textContent = ''; // limpiamos el mensaje de error.
+    }
 
-        // volvemos el texto ingresado a minuscula
-        const textoFiltrolower = textoFiltro.toLowerCase();
+    // volvemos el texto ingresado a minuscula
+    const textoFiltrolower = textoFiltro.toLowerCase();
 
-        // Filtramos las palabras originales
-        const palabrasFiltradas = palabrasOriginales.filter(palabra => {
-            // volvemos minuscula cada palabra para comparar
-            return palabra.toLowerCase().includes(textoFiltrolower);
-        });
+    // Filtramos las palabras originales
+    const palabrasFiltradas = palabrasOriginales.filter(palabra => {
+        // volvemos minuscula cada palabra para comparar
+        return palabra.toLowerCase().includes(textoFiltrolower);
+    });
 
-        mostrarPalabras(palabrasFiltradas); // actualizamos la lista
-    };
+    mostrarPalabras(palabrasFiltradas); // actualizamos la lista
+};
 
-    botonFiltrar.addEventListener('click', filtrarLista);
+botonFiltrar.addEventListener('click', filtrarLista);
 
-    // inicializamos y mostramos las palabras al cargar la pagina
-    mostrarPalabras(palabrasOriginales);
-});
+// inicializamos y mostramos las palabras al cargar la pagina
+mostrarPalabras(palabrasOriginales);
